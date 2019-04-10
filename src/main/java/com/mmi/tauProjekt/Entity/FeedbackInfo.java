3bc81423a6228a9f6177abfc87dad652bafdf92b
omerrcem <48556047+omerrcem@users.noreply.github.com>
2019-04-11 00:36:36 +0300

    Extra ordinary more development

--- src/main/java/com/mmi/tauProjekt/Entity/FeedbackInfo.java
+++ src/main/java/com/mmi/tauProjekt/Entity/FeedbackInfo.java
@@ -11,7 +11,7 @@ public class FeedbackInfo {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "feedbackid", updatable = false, nullable = false)
     private int feedbackId;
 
